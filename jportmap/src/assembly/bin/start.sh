fde681ce4990a75cc0bbd006b1b6ae5fb11d1e25
kayak <chensj@letcode.cn>
2017-08-31 19:09:31 +0800

    初始化提交

---
+++ jportmap/src/assembly/bin/start.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+$JAVA_HOME/bin/java -cp .:./portmap.jar com.kayakwise.jportmap.PortMap &
---
+++ jportmap/src/main/java/com/kayawise/jportmap/Rule.java
@@ -0,0 +1,13 @@
+package com.kayawise.jportmap;
+
+
+
+
+public class Rule {
+	public String inAddr = "";
+	public int inPort = 0;
+	public String outAddr = "";
+	public int outPort = 0;
+	
+	
+}
